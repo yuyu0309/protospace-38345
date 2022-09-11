186470191f6fb23e0e59c3796662a2a2bddd5ae3
yuyu0309 <yulego@i.softbank.jp>
2022-09-12 07:39:47 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,58 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :contributor_confirmation, only: [:edit]
+
+  def index
+    @prototypes = Prototype.includes(:user)
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments
+  end
+
+  def edit
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def contributor_confirmation
+    unless current_user == @prototype.user
+      redirect_to root_path
+    end
+  end
+end
